0d055d873ee8e32e34efe17afebdf376a77383a9
rajdeepak27 <127826412+rajdeepak27@users.noreply.github.com>
2023-07-27 13:49:17 +0530

    Initial commit

---
+++ Tests/special-spmTests/special_spmTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import special_spm
+
+final class special_spmTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
